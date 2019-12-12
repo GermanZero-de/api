3dc6246c5b9969d6769c2caa31f428cab467d7cc
Joachim Schirrmacher <j.schirrmacher@justso.de>
2019-12-12 17:40:38 +0100

    Allow access from everywhere for now

--- src/Server.js
+++ src/Server.js
@@ -11,14 +11,14 @@ module.exports = (store, models, logger, fetch, config) => {
   const mainRouter = require('./MainRouter')(adapters, controller, auth, logger)
   const app = express()
 
-  const origin = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : undefined
+  //const origin = process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : undefined
 
   if (!config.isProduction) {
     app.set('json spaces', 2)
   }
   app.use(bodyParser.urlencoded({extended: false}))
   app.use(bodyParser.json())
-  app.use(cors({origin}))
+  app.use(cors())
   logger.logExpressRequests(app)
   app.use('/', mainRouter)
   logger.logExpressErrors(app)
